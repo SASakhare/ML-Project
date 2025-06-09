5b07e48eea7ffc130d8cdfc60e020f518fe7fa8a
SASakhare <sakharwsejal@gmail.com>
2025-06-09 21:44:10 +0530

    "Hyper Parameter Tunning"

--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,7 @@ from sklearn.metrics import r2_score
 #* Add the root folder to the import path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomeException
 
 
@@ -38,7 +39,7 @@ def save_object(file_path,obj):
 
 ##** this function take the dict of model's ie. instances as input and  return the report of the model, with trained model
 
-def evaluate_model(X_train,y_train,X_test,y_test,models:dict):
+def evaluate_model(X_train,y_train,X_test,y_test,models:dict,param: dict):
     
     try:
         
@@ -46,6 +47,16 @@ def evaluate_model(X_train,y_train,X_test,y_test,models:dict):
         
         for model_name,model in models.items():
             
+            ##* finding the best parameter to the model :
+            # if model_name not in param:
+            #     raise CustomeException(f'Mode name :{model_name} is not preset in {param}',sys)
+            
+            gc=GridSearchCV(model,param[model_name],cv=5)
+            
+            gc.fit(X_train,y_train)
+            
+            model.set_params(**gc.best_params_)
+            # model.set_params(**gs.best_params_)
             model.fit(X_train,y_train) ## * Training the model
             
             y_train_pred=model.predict(X_train)
